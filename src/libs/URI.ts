807f7e35484cafb7e167bbe98549f9979dbd69f0
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-07-28 01:12:29 -0500

    Fix export URI file

--- src/libs/URI.ts
+++ src/libs/URI.ts
@@ -2,4 +2,6 @@ import { config } from "../config/config";
 
 const USER = encodeURIComponent(config.user_db);
 const PASS = encodeURIComponent(config.password_db);
-export const URI = `postgresql://${USER}:${PASS}@${config.host_db}:${config.port_db}/${config.name_db}`;
+const URI = `postgresql://${USER}:${PASS}@${config.host_db}:${config.port_db}/${config.name_db}`;
+
+export default URI;
