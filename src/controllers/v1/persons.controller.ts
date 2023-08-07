25e1667d08288e2a2f4e8506027becb3e96baa6f
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-08-07 11:55:17 -0500

    Update getPersons on persons controller

--- src/controllers/v1/persons.controller.ts
+++ src/controllers/v1/persons.controller.ts
@@ -1,11 +1,29 @@
 import { Request, Response } from "express";
 import PersonsService from "../../services/v1/persons.services";
 import { IPersons } from "../../interfaces";
+import { getLimitOffset } from "../../utils/getLimitOffset";
 
 const service = new PersonsService();
 
-export const getPersons = (req: Request, res: Response) => {
-  res.send("Get Persons");
+export const getPersons = async (req: Request, res: Response, next: any) => {
+  try {
+    const { l, o } = req.query;
+    const limit = getLimitOffset(l, 10);
+    const offset = getLimitOffset(o, 0);
+
+    const listPersons = await service.findAll(limit, offset);
+
+    const reslistPersons = {
+      success: true,
+      data: listPersons,
+      message: "Ok",
+      count: listPersons.length,
+    };
+
+    res.status(200).json(reslistPersons);
+  } catch (error: any) {
+    next(error);
+  }
 };
 
 export const createPerson = async (req: Request, res: Response, next: any) => {
